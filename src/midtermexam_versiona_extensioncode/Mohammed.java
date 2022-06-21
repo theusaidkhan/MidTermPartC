07ac4f9adeceb8b38796c72d1d5815082816d789
Usaid Khan <theusaidkhan@gmail.com>
2022-06-21 13:11:48 -0400

    CReate a class,name it as your own first_name.java.

---
+++ src/midtermexam_versiona_extensioncode/Mohammed.java
@@ -0,0 +1,17 @@
+
+
+package midtermexam_versiona_extensioncode;
+
+/**
+ *
+ * @author usaidkhan
+ */
+
+//Create new class TestUserProfile midterm
+public class Mohammed {
+    
+    //CReate a class  ,name it as your own first_name.java . Add comments there . 
+    //Commit and push to remote repo 
+
+
+}
